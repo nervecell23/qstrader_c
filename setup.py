a9f503cf7b8591a669ec29a7207eff652b2a8e9d
Chuan Cheng <c_chuan23@hotmail.com>
2020-07-05 22:02:55 +0100

    WIP update setup

--- setup.py
+++ setup.py
@@ -1,97 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from os import path
-import io
-
-here = path.abspath(path.dirname(__file__))
-
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    print("Can't import pypandoc - using README.md without converting to RST")
-    long_description = open('README.md').read()
-
-NAME = 'qstrader'
-with io.open(path.join(here, NAME, 'version.py'), 'rt', encoding='UTF-8') as f:
-    exec(f.read())
+from setuptools import setup, find_packages  
 
 setup(
-    name=NAME,
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    #version='0.0.2',
-    version=__version__,
-
-    description='QuantStart.com - Advanced Trading Infrastructure',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url=__url__,
-
-    # Author details
-    author=__author__,
-    author_email=__email__,
-
-    # Choose your license
-    license=__license__,
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Environment :: Console',
-        #'Topic :: Software Development :: Build Tools',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Cython',
-
-        'Programming Language :: Python',
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.2',
-        #'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-    ],
-
-    # What does your project relate to?
-    keywords='python trading technical analysis backtesting',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
+    name="qstrader-customised",
+    author="Michael Halls-Moore",
+    license="MIT",
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pandas'],
+    install_requires=[
+        "Click==7.1.2",
+        "matplotlib==3.0.3",
+        "numpy==1.18.4",
+        "pandas==1.0.3",
+        "seaborn==0.10.1"],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require = {
-        'dev': ['check-manifest', 'nose'],
-        'test': ['coverage', 'nose'],
+        'dev': ['check-manifest', 'pytest'],
+        'test': ['coverage', 'pytest'],
     },
 
     # If there are data files included in your packages that need to be
